153c41f65016620fe693587f1281837273e6a0f3
Szabolcs Szilagyi <sszilagyi@kiwipowered.com>
2020-07-16 19:09:40 +0100

    bump version to 0.1.3

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ fi
 declare -A version_url_map
 version_url_map["0.1.1"]="https://www.dropbox.com/s/2plv7ixju61gbk7/expose-to-the-light_2.13-0.1.1.jar?dl=0"
 version_url_map["0.1.2"]="https://www.dropbox.com/s/mhcdacxptw8yfyy/expose-to-the-light_2.13-0.1.2.jar?dl=0"
+version_url_map["0.1.3"]="https://www.dropbox.com/s/w0fsut2yhp15x53/expose-to-the-light_2.13-0.1.3.jar?dl=0"
 ##_new_version_url_map_here
 
 echo "Downloading artifact..."
