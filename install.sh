b594d8b8febf3a6d4e3201045b5ba5a18e8a6190
Szabolcs Szilagyi <sszilagyi@kiwipowered.com>
2020-07-16 11:43:59 +0100

    improve install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 version=$1
 
-if [[ -n $(which java) ]]; then
+if [[ ! -n $(which java) ]]; then
   echo "Installing jdk as did not found it on this machine..."
   sudo apt-get update
   sudo apt-get install default-jdk
@@ -10,6 +10,9 @@ fi
 
 echo "Downloading artifact..."
 wget https://www.dropbox.com/s/2plv7ixju61gbk7/expose-to-the-light_2.13-"$version".jar
+
+echo "Artifact's Manifest file:"
+cmd java xf expose-to-the-light_2.13-"$version".jar META-INF/MANIFEST.MF && cat META-INF/MANIFEST.MF
 
 echo "Installing auto-runner..."
 
