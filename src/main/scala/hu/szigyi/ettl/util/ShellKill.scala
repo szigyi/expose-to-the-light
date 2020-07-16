e525935f43ed53741906601c4b0fd4ac2fa5f5c7
Szabolcs Szilagyi <sszilagyi@kiwipowered.com>
2020-07-16 19:06:54 +0100

    implement kill gphoto background processes

--- src/main/scala/hu/szigyi/ettl/util/ShellKill.scala
+++ src/main/scala/hu/szigyi/ettl/util/ShellKill.scala
@@ -1,5 +1,9 @@
 package hu.szigyi.ettl.util
 
+import sys.process._
+
 class ShellKill {
-  def killGPhoto2Processes: Unit = ???
+  def killGPhoto2Processes: Unit = {
+    "pkill -9 -f gphoto" !
+  }
 }
