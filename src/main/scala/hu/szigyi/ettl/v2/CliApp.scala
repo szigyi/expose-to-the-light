b7f8da406e22d1178d284485743983de1795e571
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2021-02-12 21:29:47 +0000

    done 2. task
    add 4.

--- src/main/scala/hu/szigyi/ettl/v2/CliApp.scala
+++ src/main/scala/hu/szigyi/ettl/v2/CliApp.scala
@@ -9,8 +9,9 @@ import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
 // 1: Can capture image and download it so can view it from computer
-// TODO 2: Can capture consecutive images without camera failure or black screen or camera lag
+// 2: Can capture consecutive images without camera failure or black screen or camera lag
 // 3: Can successfully change settings of camera before capturing the image
+// TODO 4: Can download lower resolution of image from camera
 
 object CliApp extends IOApp with StrictLogging {
   override def run(args: List[String]): IO[ExitCode] = {
@@ -22,7 +23,7 @@ object CliApp extends IOApp with StrictLogging {
 
     IO.fromTry(ettl.execute(numberOfCaptures, interval)).attempt.map {
       case Right(imagePaths) =>
-        logger.info(s"App finished: ${imagePaths.mkString("\n")}")
+        logger.info(s"App finished: \n${imagePaths.mkString("\n")}")
         Success(imagePaths)
       case Left(exception) =>
         logger.error(s"App failed", exception)
