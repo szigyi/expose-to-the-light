d010c1d11c0583aecac9f3dd1debf548c2fbe571
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2021-02-12 20:12:52 +0000

    use name generator, simple counter
    so we can have unique files names during one session

--- src/main/scala/hu/szigyi/ettl/v2/EttlApp.scala
+++ src/main/scala/hu/szigyi/ettl/v2/EttlApp.scala
@@ -6,7 +6,7 @@ import hu.szigyi.ettl.util.ShellKill
 import hu.szigyi.ettl.v2.CameraHandler.{connectToCamera, takePhoto}
 import hu.szigyi.ettl.v2.CliApp.AppConfiguration
 
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
 import scala.util.Try
 
 class EttlApp(appConfig: AppConfiguration) extends StrictLogging {
@@ -20,8 +20,8 @@ class EttlApp(appConfig: AppConfiguration) extends StrictLogging {
 
   private def scheduledCaptures(camera: GCamera, config: GConfiguration): Try[Seq[Path]] =
     for {
-      first  <- capture(camera, config, appConfig.imageBasePath.resolve("IMG_1.CR2"), Some(SettingsCameraModel(Some(1d / 100d), Some(400), Some(2.8))))
-      second <- capture(camera, config, appConfig.imageBasePath.resolve("IMG_2.CR2"), Some(SettingsCameraModel(Some(1d), Some(100), Some(2.8))))
+      first  <- capture(camera, config, appConfig.imageBasePath.resolve(imageNameGenerator), Some(SettingsCameraModel(Some(1d / 100d), Some(400), Some(2.8))))
+      second <- capture(camera, config, appConfig.imageBasePath.resolve(imageNameGenerator), Some(SettingsCameraModel(Some(1d), Some(100), Some(2.8))))
     } yield Seq(first, second)
 
   private def capture(camera: GCamera, config: GConfiguration, imagePath: Path, c: Option[SettingsCameraModel]): Try[Path] =
@@ -53,5 +53,11 @@ class EttlApp(appConfig: AppConfiguration) extends StrictLogging {
       if (changes.nonEmpty) config.apply
       else Try()
     })
+  }
+
+  var counter = 0
+  private def imageNameGenerator: Path = {
+    counter = counter + 1
+    Paths.get(s"IMG_$counter.CR2")
   }
 }
--- src/test/scala/hu/szigyi/ettl/v2/EttlAppSpec.scala
+++ src/test/scala/hu/szigyi/ettl/v2/EttlAppSpec.scala
@@ -1,5 +1,6 @@
 package hu.szigyi.ettl.v2
 
+import hu.szigyi.ettl.v2.CliApp.AppConfiguration
 import hu.szigyi.ettl.v2.Fixtures.{capturedConfiguration, capturedFile}
 import org.gphoto2.GPhotoException
 import org.scalatest.freespec.AnyFreeSpec
@@ -14,11 +15,11 @@ class EttlAppSpec extends AnyFreeSpec with Matchers {
     "Normal Scenario" - {
       "capture image and returns them" in {
         val configuration = capturedConfiguration
-        val result = EttlApp.runEttl(new GCamera {
+        val result = new EttlApp(AppConfiguration(Paths.get("/"))).execute(new GCamera {
           override def initialize: Try[Unit] = Try()
           override def newConfiguration: Try[GConfiguration] = configuration
           override def captureImage: Try[GFile] = capturedFile
-        }, Paths.get("/"))
+        })
 
         result shouldBe a[Success[_]]
         result.get shouldBe Seq(Paths.get("/IMG_1.CR2"), Paths.get("/IMG_2.CR2"))
@@ -36,12 +37,12 @@ class EttlAppSpec extends AnyFreeSpec with Matchers {
 
     "Error Scenario" - {
       "when cannot connect to the camera then fast fail and return error" in {
-        val result = EttlApp.runEttl(new GCamera {
+        val result = new EttlApp(AppConfiguration(Paths.get("/"))).execute(new GCamera {
           override def initialize: Try[Unit] =
             Failure(new GPhotoException("gp_camera_init failed with GP_ERROR_MODEL_NOT_FOUND #-105: Unknown model", -105))
           override def newConfiguration: Try[GConfiguration] = ???
           override def captureImage: Try[GFile] = ???
-        }, Paths.get("/"))
+        })
 
         result shouldBe a[Failure[_]]
         result.failed.get.getMessage shouldBe "gp_camera_init failed with GP_ERROR_MODEL_NOT_FOUND #-105: Unknown model"
