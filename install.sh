1180613d1668e9ce232202d1a78d2ce8dcc41d0a
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2021-02-18 22:07:24 +0000

    deployed 0.1.5 version

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ version_url_map["0.1.1"]="https://www.dropbox.com/s/2plv7ixju61gbk7/expose-to-th
 version_url_map["0.1.2"]="https://www.dropbox.com/s/mhcdacxptw8yfyy/expose-to-the-light_2.13-0.1.2.jar?dl=0"
 version_url_map["0.1.3"]="https://www.dropbox.com/s/7fxahbugo7ky145/expose-to-the-light_2.13-0.1.3.jar?dl=0"
 version_url_map["0.1.4"]="https://www.dropbox.com/s/kythpz1w9nu968c/expose-to-the-light_2.13-0.1.4.jar?dl=0"
+version_url_map["0.1.5"]="https://www.dropbox.com/s/9prulf84m9k1fh0/expose-to-the-light_2.13-0.1.5.jar?dl=0"
 ##_new_version_url_map_here
 
 echo "Downloading artifact..."
