f588de3687839dc367cc8ac3d1bd4605e86d9375
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2021-03-03 20:36:26 +0000

    number of images is coming from command line

--- src/main/scala/hu/szigyi/ettl/v2/CliApp.scala
+++ src/main/scala/hu/szigyi/ettl/v2/CliApp.scala
@@ -22,29 +22,33 @@ import scala.util.{Failure, Success, Try}
 // TODO 10: webapp can start and stop systemd process
 // TODO 11: webapp can view the log
 // 12: camera settings for capture is optional - just trigger capture without overwrite settings in camera
-// TODO 13: can set number of images to be taken from command line
+// 13: can set number of images to be taken from command line
 // 14: capture image when schedule starts, do not wait until the first schedule finishes to trigger capture
+// TODO 15: use named arguments to get the command line args
+// TODO 16: interval is coming from command line as well
+// TODO 17: use better logging to inform user -> useful for task 11
 
 object CliApp extends IOApp with StrictLogging {
   override def run(args: List[String]): IO[ExitCode] = {
-    if (args.size != 2) {
+    if (args.size != 3) {
       logger.error("Base Path as first program argument is missing")
       logger.error("Set Settings as second program argument is missing")
+      logger.error("Number of images to capture as third program argument is missing")
       IO.pure(ExitCode.Error)
     } else {
       val basePath = args(0)
       val setSettings = args(1).toBoolean
-      runApp(basePath, setSettings)
+      val numberOfCaptures = args(2).toInt
+      runApp(basePath, setSettings, numberOfCaptures)
         .as(ExitCode.Success)
     }
   }
 
-  private def runApp(basePath: String, setSettings: Boolean): IO[Try[Seq[Path]]] = {
+  private def runApp(basePath: String, setSettings: Boolean, numberOfCaptures: Int): IO[Try[Seq[Path]]] = {
     val clock = Clock.systemDefaultZone()
     val appConfig = AppConfiguration(Paths.get(basePath))
     val ettl = new EttlApp(appConfig, new GCameraImpl, new SchedulerImpl(clock, 100.milliseconds))
     val setting = if (setSettings) Some(SettingsCameraModel(Some(1d / 100d), Some(400), Some(2.8))) else None
-    val numberOfCaptures = 3
     val interval = 5.seconds // TODO: validate interval should not be less than 1 milliseconds
 
     IO.fromTry(ettl.execute(setting, numberOfCaptures, interval)).attempt.map {
