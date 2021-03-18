08858fc9bf6d7a3ee88078c4c8f0d24830cbbe77
Sabi <szigyi.szogyi@gmail.com>
2021-03-18 20:23:29 +0000

    more todos

--- src/main/scala/hu/szigyi/ettl/v2/app/CliApp.scala
+++ src/main/scala/hu/szigyi/ettl/v2/app/CliApp.scala
@@ -37,6 +37,7 @@ import scala.util.{Failure, Success, Try}
 // 20: remove unnecessary info from logs
 // TODO 21: store files of one session in a dedicated timestamped directory
 // TODO 22: make script that installs the app and make it runnable from commandline to macOS and debian
+// TODO 23: can provide camera's settings for every capture (evaluated at the time of capture if it is possible)
 
 object CliApp extends IOApp with StrictLogging {
   override def run(args: List[String]): IO[ExitCode] = {
