2efdf3c1ff8316de4220885ba833ab93037f9fb0
Sabi <szigyi.szogyi@gmail.com>
2021-03-19 19:24:09 +0000

    fix app finish log

--- src/main/scala/hu/szigyi/ettl/app/CliEttlApp.scala
+++ src/main/scala/hu/szigyi/ettl/app/CliEttlApp.scala
@@ -81,7 +81,7 @@ object CliEttlApp extends IOApp with StrictLogging {
 
     IO.fromTry(ettl.execute(setting, numberOfCaptures, interval)).attempt.map {
       case Right(imagePaths) =>
-        logger.info(s"App finished:")
+        logger.info(s"App finished")
         Success(imagePaths)
       case Left(exception) =>
         logger.error(s"App failed", exception)
